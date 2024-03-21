b12cc0d1b9d0f5811f8aafd440d8bb6d7ed96dc6
Mahesh Sangle <maheshsangle431@gmail.com>
2024-03-21 08:03:17 +0530

    signin route added

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const router = express.Router();
 const signUpHandler = require("../Controllers/signUpHandler");
+const handleSignIn = require("../Controllers/signInHandler");
+
+const router = express.Router();
 
 router.post("/signUp", signUpHandler);
+router.post("/signin", handleSignIn);
 
 module.exports = router;
