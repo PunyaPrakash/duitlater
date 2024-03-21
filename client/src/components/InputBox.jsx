4d7c606cff2db7814ab32104d15d1862da2a388a
PunyaPrakash <punya5759@gmail.com>
2024-03-22 04:31:18 +0530

    added axios requests

--- client/src/components/InputBox.jsx
+++ client/src/components/InputBox.jsx
@@ -3,7 +3,7 @@ function InputBox(props) {
     <input
       type={props.type}
       className={
-        'outline-none rounded-full m-2 p-1 pl-4 text-slate-700 text-base focus:placeholder:text-slate-700 focus:cursor-none ' +
+        'outline-none rounded-full m-2 p-1 pl-4 text-slate-700 text-base focus:placeholder:text-slate-700 focus:cursor-none border-red-600 ' +
         props.className
       }
       name={props.name}
@@ -12,6 +12,7 @@ function InputBox(props) {
       onChange={props.onChange}
       autoComplete="off"
       onKeyDown={props.onKeyDown}
+      required={props.required}
     />
   );
 }
